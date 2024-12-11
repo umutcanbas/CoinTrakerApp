a3f65734784665154bf438f7dd9f314acec7c79c
umutcanbas <umutbas57@gmail.com>
2024-12-11 23:48:13 +0300

    Add useFetchCurrency hook for fetching currency conversion data

---
+++ src/hooks/useFetchCurrency.js
@@ -0,0 +1,19 @@
+/* export const getCurrency = async page => {
+  try {
+    const response = await fetch(
+      `
+        https://api.currencylayer.com/convert?access_key=feb9630e0d0fb98a57d28e10918222dc&from=USD&to=GBP&amount=10&date=2024-11-11
+        `,
+    );
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    return json;
+  } catch (error) {
+    console.log('getData Error: ', error);
+    return [];
+  }
+}; */
