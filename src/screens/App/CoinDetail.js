2a6dbf9db07dc1d74e3207bccab1f0e563f45d33
umutcanbas <umutbas57@gmail.com>
2024-12-19 20:02:56 +0300

    Refactor CoinDetail component to remove unused currency fetching logic and simplify price display

--- src/screens/App/CoinDetail.js
+++ src/screens/App/CoinDetail.js
@@ -1,5 +1,5 @@
 import {Image, SafeAreaView, StyleSheet, Text, View} from 'react-native';
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 
 import TopMenu from '../../components/TopMenu';
 
@@ -7,12 +7,8 @@ import {useDispatch, useSelector} from 'react-redux';
 
 import {changeFavoriteList} from '../../redux/slice';
 
-import {changeCurrency} from '../../hooks/useFetchCurrency';
-
 const CoinDetail = ({route, navigation}) => {
   const data = route.params?.item;
-
-  const [multiplier, setMultiplier] = useState();
 
   const dispatch = useDispatch();
 
@@ -28,25 +24,13 @@ const CoinDetail = ({route, navigation}) => {
     dispatch(changeFavoriteList(data));
   };
 
-  const currentCurrency = useSelector(state => state.slice.currentCurrency);
-
-  const fetchCurrency = async () => {
-    try {
-      const newMultiplier = await changeCurrency(currentCurrency);
-      setMultiplier(newMultiplier.result);
-    } catch (error) {
-      console.log('fetchCurrency error ', error);
-    }
-  };
-
-  useEffect(() => {
-    fetchCurrency();
-  }, [currentCurrency]);
-
-  const infoData = [ 
-    {label: 'Current Price', value: (data.current_price * multiplier).toLocaleString()},
-    {label: '24h High', value: (data.high_24h * multiplier).toLocaleString()},
-    {label: '24h Low', value: (data.low_24h * multiplier).toLocaleString()},
+  const infoData = [
+    {
+      label: 'Current Price',
+      value: data.current_price.toLocaleString(),
+    },
+    {label: '24h High', value: data.high_24h.toLocaleString()},
+    {label: '24h Low', value: data.low_24h.toLocaleString()},
     {label: '24h Price Change', value: data.price_change_24h.toFixed(2)},
     {
       label: '24h Price Change %',
